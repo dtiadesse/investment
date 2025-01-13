c15e232b0b2c4b03d77363cf3fb52fd30926960d
manisha <manishamandaliya02@gmail.com>
2025-01-14 00:23:47 +0530

    done assestmentcriteria edit table

--- components/InvestmentAssessmentCriteria.vue
+++ components/InvestmentAssessmentCriteria.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex justify-between items-center">
         <div class="text-2xl font-bold p-4">Investment Assessment Criteria</div>
-        <div class="flex items-center">
-            <div class="px-4 py-2 mr-2 font-bold flex items-center border-2 border-green-600 bg-green-50 text-black text-center rounded"  role="button">
+        <div class="flex items-center" v-if="!isEditing">
+            <div class="px-4 py-2 mr-2 font-bold flex items-center border-2 text-black text-center rounded"  :class="(OverallStatus === 'Active/Green')?'border-green-600 bg-green-50':(OverallStatus === '/Amber')?'border-yellow-600 bg-yellow-50':(OverallStatus === 'High Risk / red')?'border-red-600 bg-red-50':(OverallStatus === 'Neutral/Gray')?'border-gray-600 bg-gray-50':' '" role="button">
               <p class="pr-3">Overall Status</p>
-              <p class="w-2 h-2 bg-green-500 rounded-full mr-2"></p>
-              <p>Active/Green</p>
+              <p class="w-2 h-2 rounded-full mr-2" :class="(OverallStatus === 'Active/Green')?'bg-green-500':(OverallStatus === '/Amber')?'bg-yellow-500':(OverallStatus === 'High Risk / red')?'bg-red-500':(OverallStatus === 'Neutral/Gray')?'bg-gray-500':''"></p>
+              <p>{{OverallStatus}}</p>
             </div>
             <button
               class="text-black flex items-center border-2 border-gray-300 font-bold rounded text-sm px-4 py-2 me-2"
@@ -27,37 +27,192 @@
                     />
                     <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
                   </svg>
-              <p class="text-base">Edit Table</p>
+              <p class="text-base" @click="editTable()">Edit Table</p>
+            </button>         
+        </div>
+        <div class="flex items-center" v-else>
+            <div class="px-4 py-2 mr-2 font-bold flex items-center text-black text-center"  role="button">
+              <p class="pr-3">Overall Status</p>
+              <select
+              v-model="OverallStatus"
+              class="w-52 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            >
+            <option value="Active/Green">Active/Green</option>
+            <option value="/Amber">/Amber</option>
+            <option value="High Risk / red">High Risk / red</option>
+            <option value="Neutral/Gray">Neutral/Gray</option>
+            </select>
+            </div>
+            <button
+              class="text-black flex items-center border-2 border-gray-300 font-bold rounded text-sm px-4 py-2 me-2"
+            >
+              <p class="text-base" @click="cancelEdit()">Cancel</p>
+            </button>  
+            <button
+              class="text-white flex items-center border-2 bg-blue-400 border-blue-400 font-bold rounded text-sm px-4 py-2 me-2"
+            >
+              <p class="text-base" @click="saveEditTable()">Save Changes</p>
             </button>         
         </div>
 
       </div>
       <div class="flex py-2 px-4 border-b"  v-for="item in tableValue">
-        <div class="w-60 flex items-center">{{item.BusinessIntegration}}</div>
-        <div class="w-1/4 flex items-center"><p class="w-2 h-2 bg-green-500 rounded-full mr-2"></p><p>{{item.Status}}</p></div>
+          <div v-if="!isEditing" class="w-72 flex items-center mr-4">
+            {{item.BusinessIntegration}}
+          </div>
+          <div v-else class="w-72 flex items-center mr-4">
+            <select
+              v-model="item.BusinessIntegration"
+              class="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            >
+            <option value="Leadership">Leadership</option>
+            <option value="Risk /Return">Risk /Return</option>
+            <option value="Financial Returns">Financial Returns</option>
+            <option value="Financial Impact">Financial Impact</option>
+            <option value="Functional Integration / Implementation">Functional Integration / Implementation</option>
+            <option value="Business Integration / Implementation">Business Integration / Implementation</option>
+            </select>
+          </div>
+          <div v-if="!isEditing"  class="w-60 flex items-center mr-4">
+            {{item.InitialScore}}
+          </div>
+          <div v-else  class="w-60 flex items-center mr-4">
+            <input
+              type="text"
+              v-model="item.InitialScore"
+              class="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <div v-if="!isEditing" class="w-60 flex items-center mr-4" :class="(item.Status === 'Active/Green')?'text-green-500':(item.Status === '/Amber')?'text-red-500':(item.Status === 'High Risk / red')?'text-red-500':(item.Status === 'Neutral/Gray')?'text-gray-500':''">
+            {{item.CurrentScore}} 
+            <span v-if="item.Status ==='Active/Green'"> {{'↑'}} 
+            </span>
+            <span v-else-if="item.Status ==='High Risk / red' || item.Status ==='/Amber'">
+             {{'↓' }} 
+             </span>
+             <span v-else>
+              {{'' }} 
+             </span>
+          </div>
+          <div v-else class="w-60 flex items-center mr-4">
+            <input
+              type="text"
+              v-model="item.CurrentScore"
+              class="w-32 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <div v-if="!isEditing"  class="w-60 flex items-center mr-4">
+            <p class="w-2 h-2 rounded-full mr-2" :class="(item.Status === 'Active/Green')?'bg-green-500':(item.Status === '/Amber')?'bg-yellow-500':(item.Status === 'High Risk / red')?'bg-red-500':(item.Status === 'Neutral/Gray')?'bg-gray-500':''"></p>
+            <p>{{item.Status}}</p>
+          </div>
+          <div v-else  class="w-60 flex items-center mr-4">
+            <select
+              v-model="item.Status"
+              class="w-32 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            >
+            <option value="Active/Green">Active/Green</option>
+            <option value="/Amber">/Amber</option>
+            <option value="High Risk / red">High Risk / red</option>
+            <option value="Neutral/Gray">Neutral/Gray</option>
+            </select>
+          </div>
         <div class="w-2/3 flex items-center">
-          <ul class="list-disc pl-6">
-            <li>
-              {{item.Risks}}
-            </li>
+          <ul class="list-disc">
+            <div v-if="!isEditing">
+              <li>{{item.Risks}}</li>
+            </div>
+            <div v-else>
+            <textarea
+              v-model="item.Risks"
+              class="w-screen px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            >{{item.Risks}}</textarea>
+            </div>
           </ul>
         </div>
       </div>
       <div class="flex py-2 px-4 border-b"  v-for="item in hiddenItems"  v-if="isExpanded">
-        <div class="w-60 flex items-center">{{item.BusinessIntegration}}</div>
-        <div class="w-1/4 flex items-center"><p class="w-2 h-2 bg-green-500 rounded-full mr-2"></p><p>{{item.Status}}</p></div>
+        <div v-if="!isEditing" class="w-72 flex items-center mr-4">
+            {{item.BusinessIntegration}}
+          </div>
+          <div v-else class="w-72 flex items-center mr-4">
+            <select
+              v-model="item.BusinessIntegration"
+              class="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            >
+            <option value="Leadership">Leadership</option>
+            <option value="Risk /Return">Risk /Return</option>
+            <option value="Financial Returns">Financial Returns</option>
+            <option value="Financial Impact">Financial Impact</option>
+            <option value="Functional Integration / Implementation">Functional Integration / Implementation</option>
+            <option value="Business Integration / Implementation">Business Integration / Implementation</option>
+            </select>
+          </div>
+          <div v-if="!isEditing"  class="w-60 flex items-center mr-4">
+            {{item.InitialScore}}
+          </div>
+          <div v-else  class="w-60 flex items-center mr-4">
+            <input
+              type="text"
+              v-model="item.InitialScore"
+              class="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <div v-if="!isEditing" class="w-52 flex items-center mr-4">
+            {{item.CurrentScore}}
+          </div>
+          <div v-else class="w-52 flex items-center mr-4">
+            <input
+              type="text"
+              v-model="item.CurrentScore"
+              class="w-32 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <div v-if="!isEditing"  class="w-60 flex items-center mr-4">
+            <p class="w-2 h-2 rounded-full mr-2" :class="(item.Status === 'Active/Green')?'bg-green-500':(item.Status === '/Amber')?'bg-yellow-500':(item.Status === 'High Risk / red')?'bg-red-500':(item.Status === 'Neutral/Gray')?'bg-gray-500':''"></p>
+            <p>{{item.Status}}</p>
+          
+          </div>
+          <div v-else  class="w-60 flex items-center mr-4">
+            <select
+              v-model="item.Status"
+              class="w-32 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            >
+            <option value="Active/Green">Active/Green</option>
+            <option value="/Amber">/Amber</option>
+            <option value="High Risk / red">High Risk / red</option>
+            <option value="Neutral/Gray">Neutral/Gray</option>
+            </select>
+          </div>
         <div class="w-2/3 flex items-center">
-          <ul class="list-disc pl-6">
-            <li>
-              {{item.Risks}}
-            </li>
+          <ul class="list-disc">
+            <div v-if="!isEditing">
+              <li>{{item.Risks}}</li>
+            </div>
+            <div v-else>
+            <textarea
+              v-model="item.Risks"
+              class="w-screen px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            >{{item.Risks}}</textarea>
+            </div>
           </ul>
         </div>
       </div>
-      <div class="flex py-2 px-4 justify-center">
+      <div class="flex py-2 px-4 justify-end">
         <button @click="toggleExpand" class="buttonExpand">    
           Expand Full List Of Risks  {{ isExpanded ? '▲' : '▼' }}
         </button>
+        <div v-if="isEditing" class="ml-4 flex items-center">
+        <button
+              class="text-black flex items-center border-2 border-gray-300 font-bold rounded text-sm px-4 py-2 me-2"
+            >
+              <p class="text-base" @click="cancelEdit()">Cancel</p>
+            </button>  
+            <button
+              class="text-white flex items-center border-2 bg-blue-400 border-blue-400 font-bold rounded text-sm px-4 py-2 me-2"
+            >
+              <p class="text-base" @click="saveEditTable()">Save Changes</p>
+            </button>
+            </div>
       </div>
 </template>
 
@@ -72,36 +227,49 @@ export default {
   },
   data() {
     return {
+      OverallStatus:'Active/Green',
       // Full list of items
       items: [
             {
               "BusinessIntegration":"Business Integration / Implementation",
               "Status":"Active/Green",
+              "InitialScore":"2.44",
+              "CurrentScore":"2.22",
               "Risks":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
             },
             {
               "BusinessIntegration":"Functional Integration / Implementation",
               "Status":"/Amber",
+              "InitialScore":"2.44",
+              "CurrentScore":"2.22",
               "Risks":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
             },
             {
               "BusinessIntegration":"Financial Impact",
               "Status":"Active/Green",
+              "InitialScore":"2.44",
+              "CurrentScore":"2.22",
               "Risks":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
             },
             {
               "BusinessIntegration":"Financial Returns",
               "Status":"High Risk / red",
+              "InitialScore":"2.44",
+              "CurrentScore":"2.22",
               "Risks":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
             },
             {
               "BusinessIntegration":"Risk /Return",
               "Status":"Neutral/Gray",
+              "InitialScore":"2.44",
+              "CurrentScore":"2.22",
               "Risks":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
             },
             {
               "BusinessIntegration":"Leadership",
               "Status":"Active/Green",
+              "InitialScore":"2.44",
+              "CurrentScore":"2.22",
               "Risks":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
             }
           ],
@@ -109,6 +277,7 @@ export default {
       isExpanded: false,
       // Number of items to show initially
       initialVisibleCount: 3,
+      isEditing:false,
     };
   },
   computed: {
@@ -126,17 +295,31 @@ export default {
   },
 
   methods:{
+    editTable(){
+      this.isEditing = !this.isEditing
+    },
     toggleExpand() {
       this.isExpanded = !this.isExpanded;
     },
+    
+    // Save the edit
+    saveEditTable() {
+      this.isEditing = false; // Exit editing mode
+    },
+    cancelEdit(){
+      this.isEditing = false;
+    }
   }
 };
 
 </script>
 
-<style>
+<style scoped>
 .buttonExpand {
   cursor: pointer;
   color:blue
+}
+.w-screen{
+  width:40vw
 }
 </style>
