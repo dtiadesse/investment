8abef31bde9c2290f89d0baf914397d8cc58da44
manisha <manishamandaliya02@gmail.com>
2025-01-23 19:52:17 +0530

    add breadcrumb

--- pages/investment/[id].vue
+++ pages/investment/[id].vue
@@ -1,5 +1,43 @@
 <template>
   <div class="p-4 bg-gray-50 min-h-screen">
+    <div class="border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 mb-3">
+      <nav class="flex p-3 text-xl" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+          <li class="inline-flex items-center">
+            <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+              <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+              </svg>
+              Home
+            </a>
+          </li>
+          <li>
+            <div class="flex items-center">
+              <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+              </svg>
+              <a href="#" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">All Investments</a>
+            </div>
+          </li>
+          <li aria-current="page">
+            <div class="flex items-center">
+              <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+              </svg>
+              <span class="ms-1 text-sm font-medium text-gray-700 md:ms-2 dark:text-gray-600">{{tableData.Title}}</span>
+            </div>
+          </li>
+          <li aria-current="page">
+            <div class="flex items-center">
+              <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+              </svg>
+              <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">Edit Page</span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+    </div>
     <!-- Header -->
     <section class="flex p-4 border border-gray-300 rounded bg-white justify-end">
       <button class="bg-white border border-gray-300 mr-3 text-black px-4 py-2 rounded hover:bg-white-700">Cancel</button>
@@ -258,15 +296,42 @@ export default {
         "November",
         "December",
       ],
+      data:[],
+      loading: true,
+      error: null,
+      tableData:[],
     };
+  },
+  watch: {
+    tableData: {
+      immediate: true,
+      handler() {
+        this.loading = false;
+      },
+    },
+  },
+  created() {
+    this.fetchInvestmentsDetails();
   },
   mounted() {
    const { id } = useRoute().params
     console.log(id); 
   },
   methods: {
+    async fetchInvestmentsDetails() {
+      try {
+        const { id } = useRoute().params
+        
+        const response = await fetch('http://localhost:3001/investments/'+id);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        this.tableData = await response.json();
+      } catch (err) {
+        console.log(err);
+      }
+    },
     addInput() {
-    console.log(this.$route.params)
       this.inputs.push({ KPIName: "" }); // Add a new input
     },
     removeInput(index) {
